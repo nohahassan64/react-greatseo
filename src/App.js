670a775fcad6d587161638ac68515bfb50c42afc
Noha Hassan <nohahassan64@gmail.com>
2020-12-17 21:45:02 +0300

    React Website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-import { BrowserRouter , Route} from 'react-router-dom'
+import { BrowserRouter as Router , Route , Switch} from 'react-router-dom'
 import NavbarSection from './components/Navbar/index';
 import Home from './components/Home/index';
 import About from './components/About/index';
@@ -15,18 +15,20 @@ import './components/MFrameWork/style.css';
 class  App extends Component {
  render() {
   return (
-    <BrowserRouter>
+    <Router>
       <NavbarSection />
+      <Switch>
       <Route exact path="/about" component={About} />
-      <Route  path="/" component={Home} />
       <Route path="/casestudy" component={CaseStudy} />
       <Route path="/services" component={Services} />
       <Route path="/pricing" component={Pricing} />
       <Route path="/contact" component={ContactLink} />
+      <Route path="/" component={Home}/>
+      </Switch>
       <Clients />
       <Contact />
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
  }
 }
