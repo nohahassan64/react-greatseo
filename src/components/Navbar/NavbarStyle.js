6edd663fc3dcaf2ecc77db0aa2b0f88605e678bb
Noha Hassan <nohahassan64@gmail.com>
2020-12-19 12:02:42 +0300

    first commit

--- src/components/Navbar/NavbarStyle.js
+++ src/components/Navbar/NavbarStyle.js
@@ -1,12 +1,126 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const NavbarLink = styled(Link)`
-    color: #fff;
+export const Nav = styled.nav`
+    background: #F8F9FA;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+    font-size: 1.2rem;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+`;
+export const NavbarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+    max-width: 1300px;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 50px;
+    padding-right: 50px;
+
+@media screen and (max-width: 991px) {
+    padding-left: 30px;
+    padding-right: 30px;
+    }
+`;
+export const NavLogo = styled(Link)`
+    cursor: pointer;
+    display: flex;
+    justify-self: flex-start;
+    align-items: center;
+`;
+export const MobileIcon = styled.div`
+    display: none;
+
+@media screen and (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100% , 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #7C7C7D;
+}
+`;
+export const NavMenu = styled.ul`
+    list-style: none;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    background-color: #209FB2;
+    border-radius: 5px;
+    border: 1px solid #209FB2;
+    height: 60px;
+    margin: auto 0;
+    padding-left: 0;
+
+@media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    height: 90vh;
+    width: 100%;
+    top: 80px;
+    left: ${({click}) => (click ? 0 : '-100%')};
+    opacity: 1;
+    transition: all 0.5s ease-in-out;
+}
+`;
+export const NavItem = styled.li`
+    height: 60px;
+
+@media screen and (max-width: 960px) {
+    width: 100%;
+    padding: 40px 0;
+
+}
+`;
+export const NavLinks = styled(Link)`
     text-decoration: none;
-
+    color: #fff;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 12px 18px;
+    position: relative;
+    
+    z-index: 1;
+    :before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 0;
+    width: 100%;
+    background: #000;
+    z-index: -1;
+    transition: all .5s ease-in-out ;
+    border-radius: 5px;
+    }
     &:hover {
         color: #fff;
-        text-decoration: none;  
+        text-decoration: none;
+        :before {
+            height: 100%;
+        }
     }
-`
+
+
+@media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+    &:hover {
+        color: #fff;
+        transition: all 0.5s ease-in-out;
+    }
+}
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,42 +1,49 @@
-import React from 'react';
-import { Navbar , Container , Nav   } from 'react-bootstrap';
-import './style.css'
-import { NavbarLink } from './NavbarStyle'
+import React , { useState } from 'react';
+import { FaTimes , FaBars } from 'react-icons/fa';
+import { Nav , NavbarContainer , NavLogo , MobileIcon , NavMenu , NavItem , NavLinks } from './NavbarStyle';
 
- const NavbarSection = () => {
+const Navbar = () => {
+    const  [ click , setClick ] = useState(false);
 
-    return(
-           <Navbar bg="light" expand="lg">
-              <Container>
-                <Navbar.Brand href="#home">
-                   <img src="images/logo/logo-seo.png"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                <Nav.Link>
-                   <NavbarLink to="/">Home</NavbarLink>
-                </Nav.Link>
-                <Nav.Link>
-                   <NavbarLink to="/about">About Us</NavbarLink>
-                </Nav.Link>
-                <Nav.Link>
-                   <NavbarLink to="/casestudy">Case Study</NavbarLink>
-                   </Nav.Link>
-                <Nav.Link>
-                   <NavbarLink to="/services">Services</NavbarLink>
-                </Nav.Link>
-                <Nav.Link>
-                   <NavbarLink  to="/pricing">Pricing</NavbarLink>
-                </Nav.Link>
-                <Nav.Link>
-                   <NavbarLink to="/contact">Contact</NavbarLink>
-                </Nav.Link>
-    </Nav>
-    
-  </Navbar.Collapse>
-              </Container>
-           </Navbar>
+    const handelClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/" onClick={closeMobileMenu}>
+                        <img src="images/logo/logo-seo.png"/>
+                    </NavLogo>
+                    <MobileIcon onClick={handelClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </MobileIcon>
+                    <NavMenu onClick={handelClick} click={click}>
+                        <NavItem>
+                            <NavLinks to="/">Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/about">About Us</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/casestudy">Case Study</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/services">Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/pricing">Pricing</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/contact">Contact</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+
+                </NavbarContainer>
+            </Nav>
+            
+        </>
     )
 }
-export default NavbarSection;
+
+export default Navbar
