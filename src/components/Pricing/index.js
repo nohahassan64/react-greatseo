09874a074590079205632568d4e50fa3b94f99f5
Noha Hassan <nohahassan64@gmail.com>
2020-12-19 19:46:22 +0300

    Update

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CarouselPricing from './CarouselPricing/index';
 import PricingInfo from './PricingInfo/index';
 import HappyClients from './../Home/HappyClients/index';
-import ourpricing from './../Home/OurPricing/data'
+import {ourpricing} from './../Home/OurPricing/data'
 
 class Pricing extends Component {
   render () {
